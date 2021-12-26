db4f424cd18e44a8d830326eee551fd33546f9ba
OMEN\Muhammad <faisalprady@gmail.com>
2021-12-26 10:09:45 +0100

    update layout

--- client-site/src/components/organisms/DynamicContainer/DynamicContainer.js
+++ client-site/src/components/organisms/DynamicContainer/DynamicContainer.js
@@ -1,16 +1,46 @@
-import React from "react"
+import React, { ReactElement, useState, useCallback, useEffect } from "react"
 import styles from "./dynamicContainer.module.scss"
 import Tabs from "../../molecules/Tabs/Tabs"
 import Cart from "../../molecules/Cart/Cart"
+
+const useMediaQuery = width => {
+  const [targetReached, setTargetReached] = useState(true)
+
+  const updateTarget = useCallback(e => {
+    if (e.matches) {
+      setTargetReached(true)
+    } else {
+      setTargetReached(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width - 1}px)`)
+    media.addListener(updateTarget)
+
+    // Check on mount (callback is not called until a change occurs)
+    if (!media.matches) {
+      setTargetReached(false)
+    }
+
+    return () => media.removeListener(updateTarget)
+  }, [])
+
+  return targetReached
+}
 const DynamicContainer = () => {
+  const isBreakpoint = useMediaQuery(parseInt(styles.breakpointXS))
   return (
     <div className={styles.dynamicContainer}>
       <div className={styles.firstBlock}>
-        <Tabs />
+        <div className={styles.firstInnerContainer}>
+          <Tabs />
+        </div>
       </div>
       <div className={styles.secondBlock}>
-        {" "}
-        <Cart />
+        <div className={styles.secondInnerBlock}>
+          <Cart />
+        </div>
       </div>
     </div>
   )
