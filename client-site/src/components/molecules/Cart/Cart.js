ab51556c1470e7d0bd96306fea46108f4f2d6375
OMEN\Muhammad <faisalprady@gmail.com>
2022-01-01 14:16:03 +0100

    add dummy data to cart

--- client-site/src/components/molecules/Cart/Cart.js
+++ client-site/src/components/molecules/Cart/Cart.js
@@ -1,7 +1,37 @@
 import React from "react"
-
+import styles from "./cart.module.scss"
+const items = [
+  {
+    amount: 2,
+    name: "garlic potatoes",
+    price: 23,
+  },
+  {
+    amount: 5,
+    name: "hot potatoes",
+    price: 23,
+  },
+  {
+    amount: 1,
+    name: "teriyaki potatoes",
+    price: 23,
+  },
+]
 const Cart = () => {
-  return <div>Keranjang Belanja</div>
+  return (
+    <div className={styles.cartContainer}>
+      <div className={styles.title}>Keranjang Belanja</div>
+      <div className={styles.content}>
+        <div className={styles.orderedList}>
+          {items.map(e => (
+            <div>{e.name}</div>
+          ))}
+        </div>
+        <hr />
+        <div className={styles.detail}></div>
+      </div>
+    </div>
+  )
 }
 
 export default Cart
