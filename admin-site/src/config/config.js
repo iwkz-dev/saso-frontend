50ce1622d2ef82cd61c339a5716d9ee228ff1cfa
Muhammad Juan AKbar <mja.0812@gmail.com>
2022-03-27 09:35:42 +0200

    Update config.js
    dummy changes to trigger ci/Cd

--- admin-site/src/config/config.js
+++ admin-site/src/config/config.js
@@ -6,3 +6,5 @@ export const BASE_URL_HOST_ADMIN_EVENT = `${BASE_URL_HOST_ADMIN}/event`;
 export const BASE_URL_HOST_ADMIN_CATEGORY = `${BASE_URL_HOST_ADMIN}/category`;
 export const BASE_URL_HOST_ADMIN_ORDER = `${BASE_URL_HOST_ADMIN}/order`;
 export const BASE_URL_HOST_ADMIN_USER = `${BASE_URL_HOST_ADMIN}/user`;
+
+
