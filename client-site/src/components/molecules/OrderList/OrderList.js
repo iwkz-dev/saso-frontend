a479ea8b7e2b8c7f6b2a9c6c63e9c5802505da1d
asraavisena <asra.avisena@yahoo.com>
2022-09-11 16:14:54 +0700

    fixing download pdf

--- client-site/src/components/molecules/OrderList/OrderList.js
+++ client-site/src/components/molecules/OrderList/OrderList.js
@@ -41,7 +41,7 @@ const OrderList = ({ setOpenOrderList }) => {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
-      link.setAttribute('download', `Schedule day.pdf`);
+      link.setAttribute('download', `${orderId}.pdf`);
       document.body.appendChild(link);
       link.click();
     });
