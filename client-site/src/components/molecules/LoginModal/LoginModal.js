5fb602865a39717a51a86dadc7ce5fec5b094535
asraavisena <asra.avisena@yahoo.com>
2022-09-12 14:45:39 +0700

    add change password function

--- client-site/src/components/molecules/LoginModal/LoginModal.js
+++ client-site/src/components/molecules/LoginModal/LoginModal.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
+import axios from 'axios';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -13,14 +14,30 @@ import {
   resetLogin,
 } from '../../../stores/reducers/login';
 import styles from './loginModal.module.scss';
+import { Box } from '@mui/system';
+import { BASE_URL_HOST } from '../../../config/config';
+import { LoadingButton } from '@mui/lab';
 
 const LoginModal = () => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
     setOpen(false);
+    setIsForgotPassword(false);
     dispatch(resetLogin());
   };
+
+  // CHANGE MODAL FORGOT PASSWORD AND LOGIN
+  const [isForgotPassword, setIsForgotPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleModalForgotPassword = () => {
+    setIsForgotPassword(true);
+  };
+  useEffect(() => {
+    setIsForgotPassword(false);
+    setIsLoading(false);
+  }, []);
 
   const dispatch = useDispatch();
   const userData = useSelector(state => state.login.data.user);
@@ -41,7 +58,27 @@ const LoginModal = () => {
   };
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch(submitLogin(userData));
+    if (isForgotPassword) {
+      setIsLoading(true);
+      axios({
+        url: `${BASE_URL_HOST}/auth/forget-password`,
+        method: 'patch',
+        data: {
+          email: userData.email,
+        },
+      })
+        .then(response => {
+          if (response.status === 200) {
+            handleClose();
+            setIsLoading(false);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } else {
+      dispatch(submitLogin(userData));
+    }
   };
 
   return (
@@ -54,7 +91,20 @@ const LoginModal = () => {
         Login
       </Button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Login</DialogTitle>
+        <DialogTitle>
+          {isForgotPassword ? 'Forget Password' : 'Login'}
+        </DialogTitle>
+        {!isForgotPassword ? (
+          ''
+        ) : (
+          <Box
+            sx={{
+              marginX: '1.5em',
+            }}
+          >
+            We send you link to your email for changing your password
+          </Box>
+        )}
         <form
           id="login-form"
           onSubmit={handleSubmit}
@@ -72,31 +122,58 @@ const LoginModal = () => {
                   required
                 />
               </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  variant="outlined"
-                  type="password"
-                  required
-                />
-              </Grid>
+              {isForgotPassword ? (
+                ''
+              ) : (
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    required
+                  />
+                </Grid>
+              )}
             </Grid>
             <span className="text-xs text-red-700 ">{errorMessage}</span>
+            {isForgotPassword ? (
+              ''
+            ) : (
+              <Button
+                onClick={handleModalForgotPassword}
+                sx={{
+                  display: isForgotPassword ? 'hidden' : 'block',
+                  padding: 0,
+                  marginTop: '10px',
+                  '&:hover': {
+                    backgroundColor: '#fff',
+                  },
+                }}
+              >
+                Forget Password?
+              </Button>
+            )}
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose}>Cancel</Button>
-            <Button
-              // onClick={e => {
-              //   console.log('asdasd');
-              //   handleClose();
-              // }}
-              form="login-form"
-              type="submit"
-            >
-              Login
-            </Button>
+            {isLoading ? (
+              <LoadingButton loading variant="outlined">
+                Submit
+              </LoadingButton>
+            ) : (
+              <Button
+                // onClick={e => {
+                //   console.log('asdasd');
+                //   handleClose();
+                // }}
+                form="login-form"
+                type="submit"
+              >
+                {isForgotPassword ? 'Send Email' : 'Login'}
+              </Button>
+            )}
           </DialogActions>
         </form>
       </Dialog>
