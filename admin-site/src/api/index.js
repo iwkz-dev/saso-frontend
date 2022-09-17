8a3f34c1f883c05141c9cb7c64bf5b3128f2b998
asraavisena <asra.avisena@yahoo.com>
2022-09-17 15:32:43 +0700

    change call api function category

--- admin-site/src/api/index.js
+++ admin-site/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { BASE_URL_HOST_ADMIN_EVENT } from "../config/config";
+import { BASE_URL_HOST_ADMIN } from "../config/config";
 
 const sasoApi = axios.create({
-    baseURL: `${BASE_URL_HOST_ADMIN_EVENT}`,
+    baseURL: `${BASE_URL_HOST_ADMIN}`,
 });
 
 export default sasoApi;
--- admin-site/src/config/config.js
+++ admin-site/src/config/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL_HOST = "http://localhost:3000/api/v1";
+export const BASE_URL_HOST = "https://saso.iwkz.de/api/v1";
 export const BASE_URL_HOST_ADMIN = `${BASE_URL_HOST}/admin`;
 
 export const BASE_URL_HOST_ADMIN_MENU = `${BASE_URL_HOST_ADMIN}/menu`;
