5b3b634c546cc40c553290d97619f2b56de4c072
asraavisena <asra.avisena@yahoo.com>
2022-09-17 15:20:56 +0700

    change call api function

---
+++ admin-site/src/api/index.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { BASE_URL_HOST_ADMIN_EVENT } from "../config/config";
+
+const sasoApi = axios.create({
+    baseURL: `${BASE_URL_HOST_ADMIN_EVENT}`,
+});
+
+export default sasoApi;
