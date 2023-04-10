81f683339ff0d4d896fd4c3a30c0eaf7ea931d2b
ilhamuh97 <ilhamuh97@gmail.com>
2023-04-10 11:50:13 +0200

    move change password to backup

--- client-site/src/backup/change-password/[token].js
+++ client-site/src/backup/change-password/[token].js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import Head from 'next/head';
 import axios from 'axios';
 import Button from '@mui/material/Button';
