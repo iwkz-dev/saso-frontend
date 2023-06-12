209c784fd499e81a8a5cf016dc242b08b0a4e561
Ghopit <kesduhafidz@yahoo.de>
2023-06-12 21:12:11 +0200

    add Search bar & Scanbarcode in client

--- client-site/src/components/molecules/ProductCards.js/ProductCards.js
+++ client-site/src/components/molecules/ProductCards.js/ProductCards.js
@@ -1,13 +1,17 @@
 import { Card, Space } from "antd";
 import ProductItem from "./ProductItem/ProductItem";
 
-const ProductCards = ({ productList }) => {
+const ProductCards = ({ productList, barcode }) => {
     const { Meta } = Card;
     return (
         <Space wrap size={[24, 32]}>
-            {productList.map((product) => (
-                <ProductItem key={product.name} product={product} />
-            ))}
+            {productList
+                .filter((product) =>
+                    barcode != "" ? product.barcode === barcode : true,
+                )
+                .map((product) => (
+                    <ProductItem key={product.name} product={product} />
+                ))}
         </Space>
     );
 };
--- client-site/src/components/organisms/TokoContent/TokoContent.js
+++ client-site/src/components/organisms/TokoContent/TokoContent.js
@@ -1,17 +1,77 @@
-import React from "react";
-import { Typography } from "antd";
+import React, { useState } from "react";
+import { Tag, Typography, message } from "antd";
 import TabsComponent from "../../molecules/TabsComponent/Tabs";
 import ContentLayout from "../ContentLayout/ContentLayout";
+import BarcodeScanner from "../../atoms/BarcodeScanner/BarcodeScanner";
+import { Button, Input, Space, Modal } from "antd";
+import style from "./FormItem.module.scss";
+import { CameraOutlined } from "@ant-design/icons";
 
+const { Search } = Input;
 const TokoContent = ({ event }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [barcode, setBarcode] = useState("");
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancelModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const onSearch = (value) => console.log(value);
+
+    const getBarcode = (barcode) => {
+        message.success("Barcode has been successfully detected!");
+        setIsModalOpen(false);
+        setBarcode(barcode);
+    };
+    const onCloseBarcodeTag = () => {
+        setBarcode("");
+    };
+
+    <Space direction="vertical"></Space>;
+
     return (
         <ContentLayout hasCarousel>
-            <Typography>
-                <Typography.Title level={2} style={{ textAlign: "center" }}>
-                    Products
-                </Typography.Title>
-            </Typography>
-            <TabsComponent event={event} />
+            <Typography.Title level={2} style={{ textAlign: "center" }}>
+                Products
+            </Typography.Title>
+            <Space direction="vertical" size="middle">
+                <Space.Compact direction="horizontal">
+                    <Search
+                        placeholder="input search text"
+                        allowClear
+                        onSearch={onSearch}
+                        style={{
+                            width: 200,
+                        }}
+                    />
+                    <Button icon={<CameraOutlined />} onClick={showModal} />
+                </Space.Compact>
+                <Modal
+                    className={style.scannerModal}
+                    destroyOnClose={true}
+                    title="Scan barcode"
+                    open={isModalOpen}
+                    onOk={handleOk}
+                    onCancel={handleCancelModal}
+                >
+                    <BarcodeScanner getBarcode={getBarcode} />
+                </Modal>
+
+                {barcode ? (
+                    <Tag closable onClose={onCloseBarcodeTag}>
+                        Barcode: {barcode}
+                    </Tag>
+                ) : null}
+
+                <TabsComponent event={event} barcode={barcode} />
+            </Space>
         </ContentLayout>
     );
 };
